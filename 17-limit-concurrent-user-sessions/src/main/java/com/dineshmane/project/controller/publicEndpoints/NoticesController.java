79f5ba069ad11d8aafadc100e451b6d8d4de1750
Dinesh <dinesh30mane@gmail.com>
2026-09-02 23:19:54 +0530

    Limit concurrent sessions and prevent login when session limit is exceeded

---
+++ 17-limit-concurrent-user-sessions/src/main/java/com/dineshmane/project/controller/publicEndpoints/NoticesController.java
@@ -0,0 +1,14 @@
+package com.dineshmane.project.controller.publicEndpoints;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NoticesController {
+
+    @GetMapping("/notices")
+    public String getAllNotices(){
+        return "Hello Dinesh!, here are all the notices and articles published by us lately...";
+    }
+
+}
